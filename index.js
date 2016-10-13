69314db00d2f681fe43631983226a1a942d5d541
David Ferreira <daminufe.ti@gmail.com>
2016-10-13 16:14:37 +0200

    Test

--- index.js
+++ index.js
@@ -2,9 +2,47 @@
 
 const restify = require('restify'),
     switchLight = require('./modules/switch-light'),
-    temperature = require('./modules/temperature');
+    GrovePi = require('node-grovepi').GrovePi,
+    Board = GrovePi.board,
+    DHTDigitalSensor = GrovePi.sensors.DHTDigital;
 
 var server = restify.createServer();
+
+
+
+/* ========================================================
+ ================ TEMPERATURE BOARD =======================
+ ========================================================= */
+
+
+let board = new Board({
+        debug: true,
+        onError: err => res.send(500, err),
+        onInit: onInit
+    }),
+    temperature = board.read();
+
+function onInit (result) {
+    console.log('onInit called!!!' + time());
+    if (result) {
+        var dhtSensor = new DHTDigitalSensor(7, DHTDigitalSensor.VERSION.DHT11, DHTDigitalSensor.CELSIUS);
+
+        dhtSensor.on('change', function(sensorData) {
+            temperature = sensorData;
+        });
+
+    } else {
+        console.log(`Result: ${result}`);
+    }
+}
+
+board.init();
+
+
+function getTemperature (req, res, next) {
+    res.send(200, temperature);
+}
+
 
 /* ========================================================
 ====================== ROUTES =============================
@@ -12,7 +50,7 @@ var server = restify.createServer();
 
 server.get('/', index);
 server.post('/light/:color', switchLight);
-server.get('/temperature', temperature);
+server.get('/temperature', getTemperature);
 
 
 /* ========================================================
@@ -27,3 +65,15 @@ function index(req, res, next) {
     res.send(200, { status: 'running' });
     next();
 }
+
+function onExit(err) {
+    board.close();
+    process.removeAllListeners();
+    process.exit();
+    if (typeof err != 'undefined')
+        console.log(err);
+}
+
+// catches ctrl+c event
+process.on('SIGINT', onExit);
+
