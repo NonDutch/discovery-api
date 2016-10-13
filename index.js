e2e9e244c3275da18410505d8d5a46c07013e0ef
David Ferreira <daminufe.ti@gmail.com>
2016-10-13 17:07:47 +0200

    Test new structure

--- index.js
+++ index.js
@@ -2,45 +2,9 @@
 
 const restify = require('restify'),
     switchLight = require('./modules/switch-light'),
-    GrovePi = require('node-grovepi').GrovePi,
-    Board = GrovePi.board,
-    DHTDigitalSensor = GrovePi.sensors.DHTDigital;
+    temperatureData = require('./modules/temperature');
 
 var server = restify.createServer();
-
-
-
-/* ========================================================
- ================ TEMPERATURE BOARD =======================
- ========================================================= */
-
-
-let temperatureBoard = new Board({
-        debug: true,
-        onError: err => res.send(500, err),
-        onInit: onInit
-    }),
-    temperature = {};
-
-function onInit (result) {
-    if (result) {
-        var dhtSensor = new DHTDigitalSensor(7, DHTDigitalSensor.VERSION.DHT11, DHTDigitalSensor.CELSIUS);
-        dhtSensor.watch(500);
-
-        dhtSensor.on('change', function(sensorData) {
-            temperature = {
-                temperature: sensorData[0],
-                humidity: sensorData[1]
-            };
-        });
-
-    } else {
-        console.log(`Result: ${result}`);
-    }
-}
-
-temperatureBoard.init();
-
 
 
 /* ========================================================
@@ -49,7 +13,7 @@ temperatureBoard.init();
 
 server.get('/', index);
 server.post('/light/:color', switchLight);
-server.get('/temperature', (req, res) => { res.send(200, temperature) });
+server.get('/temperature', temperatureData.get);
 
 
 /* ========================================================
@@ -64,15 +28,3 @@ function index(req, res, next) {
     res.send(200, { status: 'running' });
     next();
 }
-
-function onExit(err) {
-    board.close();
-    process.removeAllListeners();
-    process.exit();
-    if (typeof err != 'undefined')
-        console.log(err);
-}
-
-// catches ctrl+c event
-process.on('SIGINT', onExit);
-
---
+++ modules/temperature.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const GrovePi = require('node-grovepi').GrovePi,
+    Board = GrovePi.board,
+    DHTDigitalSensor = GrovePi.sensors.DHTDigital;
+
+let temperatureBoard = new Board({
+        debug: true,
+        onError: err => res.send(500, err),
+        onInit: onInit
+    }),
+    temperature = {};
+
+function onInit (result) {
+    if (result) {
+        var dhtSensor = new DHTDigitalSensor(7, DHTDigitalSensor.VERSION.DHT11, DHTDigitalSensor.CELSIUS);
+        dhtSensor.watch(500);
+
+        dhtSensor.on('change', function(sensorData) {
+            temperature = {
+                temperature: sensorData[0],
+                humidity: sensorData[1]
+            };
+        });
+
+    } else {
+        console.log(`Result: ${result}`);
+    }
+}
+
+temperatureBoard.init();
+
+
+
+
+
+module.exports.get = function (req, res) {
+    res.send(200, req.temperature);
+};
+
+
+
+
+function onExit(err) {
+    board.close();
+    process.removeAllListeners();
+    process.exit();
+    if (typeof err != 'undefined')
+        console.log(err);
+}
+
+// catches ctrl+c event
+process.on('SIGINT', onExit);
+
